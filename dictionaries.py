645f08c490d833b4f5aca0092f06c2aaa2baf5d9
Messinias1 <you@example.com>
2020-10-13 17:54:04 -0400

    new concepts

---
+++ dictionaries.py
@@ -0,0 +1,20 @@
+
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": 'August',
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December"
+}
+
+# Keys do not have to be strings
+
+print(monthConversions.get("Dec"))
+print(monthConversions["Nov"])
